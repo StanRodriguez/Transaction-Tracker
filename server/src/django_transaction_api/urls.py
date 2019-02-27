27dee0ba1036d9f44f23f0ee3e682f5ab787d6ed
bestanly <ibestanly@gmail.com>
2019-02-27 10:34:03 -0500

    Add first view return json logic

--- server/src/django_transaction_api/urls.py
+++ server/src/django_transaction_api/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from transactions.views import transactions_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('transaction/<id>', transactions_view),
 ]
--- server/src/transactions/views.py
+++ server/src/transactions/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from transactions.models import User, Transaction
+
 
 # Create your views here.
+
+
+def transactions_view(request, id=None):
+    user = User.objects.get(id=id)
+    transaction = list(user.transaction_set.values())
+    return JsonResponse({'user': user.id, 'transactions': transaction}, safe=False)
