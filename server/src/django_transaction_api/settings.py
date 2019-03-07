21d32f23f655d1d82c916ae617e3db0a676ad1eb
bestanly <ibestanly@gmail.com>
2019-03-07 13:04:29 -0500

    Add heroku package for django, add requirements.txt

--- server/src/django_transaction_api/settings.py
+++ server/src/django_transaction_api/settings.py
@@ -1,3 +1,5 @@
+import os
+import django_heroku
 """
 Django settings for django_transaction_api project.
 
@@ -10,7 +12,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
-import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -114,7 +115,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
